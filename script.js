83d52a376a67fc8b8832a6f4022f5c52102a3f16
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2024-01-21 21:14:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+async function delayAndShowMessage(){
+	const textInput=document.getElementById('text');
+	const delayInput=document.getElementById('delay');
+	const outPut=document.getElementById('output');
+
+	const text=textInput.value.trim();
+	const delayInSec=parseInt(delayInput.value);
+
+	if(text==='' || isNaN(delayInSec) || delayInSec<0){
+		alert('Please enter valid text and delay');
+		return;
+	}
+	const delayMilliSec=delayInSec*1000;
+	await new Promise(resolve=>setTimeout(resolve,delayMilliSec));
+
+	outPut.textContent=text;
+	
+}
+document.getElementById('btn').addEventListener('click',delayAndShowMessage);
