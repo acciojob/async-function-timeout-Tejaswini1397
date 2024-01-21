4cfa0ea3f1c8568f0eb8399b48bd66f69281a093
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2024-01-21 21:35:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 //your JS code here. If required.
-    const text=document.getElementById("text");
-	const delay=document.getElementById("delay");
-   const btn=document.getElementById("btn");
-	const outPut=document.getElementById("output");
+//     const text=document.getElementById("text");
+// 	const delay=document.getElementById("delay");
+//    const btn=document.getElementById("btn");
+// 	const outPut=document.getElementById("output");
 
-async function showMessage(){
+// async function showMessage(){
 	
-	const message=text.value;
+// 	const message=text.value;
 	
-	const delayVal=delay.vlue;
-	await new Promise(resolve=>setTimeout(resolve,delayVal));
-	outPut.innerText=message;
+// 	const delayVal=delay.vlue;
+// 	 const delayInMilliseconds = delayVal * 1000;
+// 	await new Promise((resolve) => setTimeout(resolve, delayInMilliseconds));
+// 	outPut.innerText=message;
 	
+// }
+
+// btn.addEventListener('click',showMessage);
+
+const text = document.getElementById("text");
+const delay = document.getElementById("delay");
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+//your code here
+async function showMessage() {
+  const message = text.value;
+  const delayVal = delay.value;
+  await new Promise((resolve) => setTimeout(resolve, delayVal));
+  output.innerText = message;
 }
 showMessage().then(()=>{
-	outPut.innerHTML=text.value;
-})
-btn.addEventListener('click',showMessage);
+	output.innerHTML = text.value;
+});
+btn.addEventListener("click", showMessage);
